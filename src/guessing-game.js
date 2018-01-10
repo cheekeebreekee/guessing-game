89abba594e0b68ad6893dff6ca7392ddfaf72069
Yahor Simak <simak_yahor@mail.ru>
2018-01-10 21:42:00 +0300

    add implementation of guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor()
+    {
+      this.min = null;
+      this.max = null;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      return (Math.round((this.max - this.min) / 2) + this.min)
     }
 
     lower() {
-
+      this.max = Math.round((this.max - this.min) / 2) + this.min
     }
 
     greater() {
-
+      this.min = Math.round((this.max - this.min) / 2) + this.min
     }
 }
 
